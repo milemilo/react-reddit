b0af413422ddefcaa912c470ff2b9c12a378b447
Mile Milovanovic <mmilova1@gmail.com>
2019-06-01 09:56:37 -0500

    chore(app): adding react router with home route

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const Home = () => (
+  <h1>Home Page</h1>
+)
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>React Reddit</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
