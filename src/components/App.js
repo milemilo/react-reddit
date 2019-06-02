1b6dbda6b4ca65d15aefea49336a53426118e314
Mile Milovanovic <mmilova1@gmail.com>
2019-06-02 10:00:19 -0500

    chore(app): adding Nomatch compoonent to handle 404s

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Posts from './Posts'
 
+const NoMatch = () => (
+  <h1>404 Not Found</h1>
+)
+
 class App extends Component {
   render() {
     return (
@@ -10,6 +14,7 @@ class App extends Component {
           <Switch>
             <Route exact path='/' render={() => <h1>home</h1>}/>
             <Route exact path='/posts' component={Posts}/>
+            <Route component={NoMatch}/>
           </Switch>
         </BrowserRouter>
       </div>
