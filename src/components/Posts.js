e49fedc487f7de48cc1ec34c778d73f7fa75fe38
Mile Milovanovic <mmilova1@gmail.com>
2019-06-02 09:56:02 -0500

    chore(app): rendering posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,30 +1,117 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import styled from "styled-components";
+import axios from "axios";
 
-const url = 'https://www.reddit.com/r/pizza.json'
+const baseUrl = "http://www.reddit.com";
+const url = "https://www.reddit.com/r/pizza.json";
+
+const styles = {
+  "& .page-title": {
+    textAlign: "center"
+  },
+  "& .post": {
+    border: "1px solid lightGrey",
+    borderRadius: 5,
+    display: "flex",
+    marginBottom: 15,
+    maxWidth: 550,
+    marginRight: 10,
+    padding: 0
+  },
+  "& .details": {
+    flexDirection: "column",
+    paddingLeft: 10
+  },
+  "& .img-container": {
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center"
+  },
+  "& .image": {
+    borderRadius: 5
+  },
+  "& .fa-comments": {
+    marginRight: 5
+  },
+  "& .author-container": {
+    marginBottom: 10
+  },
+  "& .comments-container": {
+    marginBottom: 15
+  }
+};
+
+const StyledDiv = styled.div`${styles}`;
 
 class Posts extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       posts: []
-    }
+    };
   }
   componentDidMount() {
-    axios.get(url)
-    .then(res => {
-      console.log(res.data.data.children)
-      this.setState({ posts: res.data.data.children })
-    })
-    .catch(err => {
-      console.log(err)
-    })
+    console.log(this.props)
+    axios
+      .get(url)
+      .then(res => {
+        console.log(res.data.data.children);
+        this.setState({
+          posts: res.data.data.children.filter(item => !item.data.distinguished)
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
   render() {
-    return(
-      <h1>Posts</h1>
-    )
+    const { posts } = this.state;
+    const prefix = posts.length > 0 && posts[0].data.subreddit_name_prefixed;
+    console.log(posts);
+    return (
+      <StyledDiv>
+        <h1 className="page-title">{prefix}</h1>
+        <div className="row center-xs">
+          {posts.slice(0, 25).map((post, i) => (
+            <div
+              className="col-xs-10 col-sm-12 col-md-6 col-lg-6 post start-xs"
+              key={post.data.id}
+            >
+            {console.log(post.data)}
+              <div className="img-container">
+                {post.data.thumbnail.length > 0 &&
+                post.data.thumbnail !== "self" ? (
+                  <img className="image" src={post.data.thumbnail} />
+                ) : (
+                  <img
+                    width="143"
+                    className="image"
+                    src="/src/assets/images/reddit_default.png"
+                  />
+                )}
+              </div>
+              <div className="details">
+                <h3 className="title">
+                  <a href={`${baseUrl}${post.data.permalink}`}>
+                    {post.data.title}
+                  </a>
+                </h3>
+                <div className="author-container">
+                  <span className="author">Posted by {post.data.author}</span>
+                </div>
+                <div className="comments-container">
+                  <i className="fas fa-comments" />
+                  <a href={`${baseUrl}${post.data.permalink}`}>
+                    {post.data.num_comments} comments
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </StyledDiv>
+    );
   }
 }
 
-export default Posts
+export default Posts;
