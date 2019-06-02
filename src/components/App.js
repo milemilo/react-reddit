bc07259b275fa59c49689c28ec1fa44d9a7eaf9e
Mile Milovanovic <mmilova1@gmail.com>
2019-06-02 10:26:34 -0500

    chore(app): creating basic home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Posts from './Posts'
+import Home from './Home'
 
 const NoMatch = () => (
   <h1>404 Not Found</h1>
@@ -12,7 +13,7 @@ class App extends Component {
       <div>
         <BrowserRouter>
           <Switch>
-            <Route exact path='/' render={() => <h1>home</h1>}/>
+            <Route exact path='/' component={Home}/>
             <Route exact path='/posts' component={Posts}/>
             <Route component={NoMatch}/>
           </Switch>
