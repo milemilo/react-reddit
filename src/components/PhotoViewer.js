f362f2e83143c29a1dae623f9e43e357efe46c7a
Mile Milovanovic <mmilova1@gmail.com>
2019-06-05 08:54:28 -0500

    chore(app): adding ability to filter by group

--- src/components/PhotoViewer.js
+++ src/components/PhotoViewer.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Gallery from "react-photo-gallery";
+import { Dropdown, Loader } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
 import { arrayShuffle } from "../utils/helpers";
 import styled from 'styled-components';
 import axios from 'axios';
 
+const options = [
+  { key: 1, text: 'Pizza', value: 'Pizza' },
+  { key: 2, text: 'Ramen', value: 'ramen' },
+  { key: 3, text: 'Sushi', value: 'sushi' },
+]
+
 const styles = {
-  "& .page-title": {
+  "& .nav-wrapper": {
     marginBottom: 20,
     marginTop: 20
   }
@@ -17,7 +25,8 @@ class PhotoViewer extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      images: []
+      images: [],
+      selectedFilter: ""
     }
   }
   componentDidMount() {
@@ -42,6 +51,7 @@ class PhotoViewer extends React.Component {
       if(post.data.thumbnail !== "" && post.data.thumbnail !== "self") {
         this.setState({
           images: [...this.state.images, {
+            group: post.data.subreddit,
             src: post.data.url,
             title: post.data.title,
             width: 1,
@@ -51,18 +61,37 @@ class PhotoViewer extends React.Component {
       }
     })
   }
+  handleSelect = (e, { value }) => this.setState({ selectedFilter: value })
   render() {
-    const { images } = this.state;
-    // console.log(this.state.images)
+    const { images, selectedFilter } = this.state;
+    const filteredImages = images.filter(img => img.group === selectedFilter)
+
     arrayShuffle(images)
     return(
       <StyledDiv>
-        <div className="row center-xs">
-          <div className="col-xs-12 page-title">
-            <h1>Photo Viewer</h1>
+        <div className="row">
+          <div className="col-xs-12 nav-wrapper">
+            <div className="row around-xs center-xs">
+              <div className="col-xs-3">
+                <h3><Link to="/">Back to Home</Link></h3>
+              </div>
+              <div className="col-xs-3">
+                <h1>Photo Viewer</h1>
+              </div>
+              <div className="col-xs-3">
+                <h3>Filter by...</h3>
+                <Dropdown
+                  clearable
+                  placeholder='Select Group'
+                  options={options}
+                  selection
+                  onChange={this.handleSelect}
+                />
+              </div>
+            </div>
           </div>
         </div>
-        <Gallery photos={images} />
+        <Gallery photos={selectedFilter.length ? filteredImages : images} />
       </StyledDiv>
     )
   }
