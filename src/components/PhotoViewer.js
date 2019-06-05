5ab5da6964762b0f0c5be072df192e113b16bd63
Mile Milovanovic <mmilova1@gmail.com>
2019-06-05 07:26:29 -0500

    chore(app): fetching and storing images from 3 subreddits

--- src/components/PhotoViewer.js
+++ src/components/PhotoViewer.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 class PhotoViewer extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      images: []
+    }
+  }
+  componentDidMount() {
+    axios.get('http://www.reddit.com/r/pizza.json')
+      .then((res) => {
+        this.aggData(res.data.data.children)
+        return axios.get('http://www.reddit.com/r/sushi.json');
+      })
+      .then((res) => {
+        this.aggData(res.data.data.children)
+        return axios.get('http://www.reddit.com/r/food.json');
+      })
+      .then((res) => {
+        this.aggData(res.data.data.children)
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+  }
+  aggData(posts) {
+    posts.map(post => {
+      if(post.data.thumbnail !== "" && post.data.thumbnail !== "self") {
+        this.setState({
+          images: [...this.state.images, {
+            src: post.data.url,
+            title: post.data.title
+          }]
+        })
+      }
+    })
+  }
   render() {
+    console.log(this.state.images)
     return(
       <h1>Photo Viewer</h1>
     )
