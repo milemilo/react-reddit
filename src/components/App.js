edbff09eea69ef2d5b7550c37ff0b26ccf6af36f
Mile Milovanovic <mmilova1@gmail.com>
2019-06-04 21:55:16 -0500

    chore(app): creating photo viewer route

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,10 @@ const NoMatch = () => (
   <h1>404 Not Found</h1>
 )
 
+const PhotoViewer = () => (
+  <h1>Photo Viewer</h1>
+)
+
 class App extends Component {
   render() {
     return (
@@ -15,6 +19,7 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Home}/>
             <Route exact path='/posts' component={Posts}/>
+            <Route exact path='/photos' component={PhotoViewer}/>
             <Route component={NoMatch}/>
           </Switch>
         </BrowserRouter>
